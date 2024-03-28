7dc5d3bf49aa8f7478af415983af16eeef5c0337
William Cheng <56508634+ch3ng625@users.noreply.github.com>
2024-03-28 12:38:25 +1100

    Update demo.cs

--- demo.cs
+++ demo.cs
@@ -19,7 +19,7 @@ namespace SecretsDemo
         public const string Production = "Production";
         public const string ProductionPass = "ghk!7^kh]9U(C8@}q?]?";
         public const string ProductionSiteName = "production.site";
-        public const string qasdfkkks = "fL/&398{xvQU";
+        public const string qasdfkkks = "26*8II(VWJ3*ZmQ&a_";
     }
 
     class Secrets
